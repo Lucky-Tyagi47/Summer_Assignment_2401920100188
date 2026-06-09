8031e89f626a3f2bc80d56e7f3718723e52a8b21
Lucky-Tyagi47 <tyagij787@gmail.com>
2026-06-09 09:26:56 +0530

    Implement RansomNote solution to check constructibility

---
+++ Week-2/Day-1/RansomNote.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int n=ransomNote.size();
+        int m=magazine.size();
+        if(m<n){
+            return false;
+        }
+        int count[26]={0};
+        for(char c:ransomNote){
+            count[c-'a']--;
+        }
+        for(char c:magazine){
+            count[c-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(count[i]<0){
+                return false;
+            }
+        }
+        return true;
+
+        
+    }
+};
