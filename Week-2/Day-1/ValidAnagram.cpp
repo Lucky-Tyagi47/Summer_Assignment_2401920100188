1673c3d78622494cd678a5d152b36f51590013fb
Lucky-Tyagi47 <tyagij787@gmail.com>
2026-06-09 09:23:30 +0530

    Implement isAnagram function to check anagrams

---
+++ Week-2/Day-1/ValidAnagram.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        if(n!=m){
+            return false;
+        }
+       int count[26]={0};
+       for(char c:s){
+        count[c-'a']++;
+       }
+       for(char c:t){
+        count[c-'a']--;
+       }
+       for(int i=0;i<26;i++){
+        if(count[i]!=0){
+            return false;
+        }
+       }
+       return true;
+       }
+        
+    
+};
